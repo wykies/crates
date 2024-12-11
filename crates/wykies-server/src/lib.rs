e8241dee54f768dd4d8078ad6206a431802bed95
One <43485962+c-git@users.noreply.github.com>
2024-12-11 18:24:43 -0500

    refactor: move initialization code into module

--- crates/wykies-server/src/lib.rs
+++ crates/wykies-server/src/lib.rs
@@ -4,13 +4,8 @@
 
 #![warn(unused_crate_dependencies)]
 
-use std::future::Future;
-
-use serde::de::DeserializeOwned;
-use tracked_cancellations::{CancellationTracker, TrackedCancellationToken};
-use wykies_shared::telemetry;
-
 mod configuration;
+mod init;
 mod macros;
 pub mod plugin;
 pub mod ws;
@@ -19,51 +14,4 @@ pub mod ws;
 pub mod db_types;
 
 pub use configuration::{get_configuration, Configuration, DatabaseSettings, WebSocketSettings};
-
-// TODO 1: Move server init into module
-pub struct ServerInit<T: Clone> {
-    pub cancellation_token: TrackedCancellationToken,
-    pub cancellation_tracker: CancellationTracker,
-    pub configuration: Configuration<T>,
-}
-
-// TODO 5: Add example to docstring of calling with stdout
-/// Does the initial prep before starting to build the server
-pub fn server_init<Sink, S, T>(default_env_filter_directive: S, sink: Sink) -> ServerInit<T>
-where
-    Sink: for<'a> tracing_subscriber::fmt::MakeWriter<'a> + Send + Sync + 'static,
-    S: AsRef<str>,
-    T: Clone + DeserializeOwned,
-{
-    let (cancellation_token, cancellation_tracker) = TrackedCancellationToken::new();
-    let subscriber =
-        telemetry::get_subscriber("wic_server".into(), default_env_filter_directive, sink);
-    telemetry::init_subscriber(subscriber).expect("failed to initialize the subscriber");
-    let configuration = get_configuration::<T>().expect("failed to read configuration.");
-    ServerInit {
-        cancellation_token,
-        cancellation_tracker,
-        configuration,
-    }
-}
-
-pub trait ServerTask {
-    fn name() -> &'static str;
-
-    fn run(
-        self,
-        cancellation_token: TrackedCancellationToken,
-    ) -> impl Future<Output = anyhow::Result<()>> + Send
-    where
-        Self: Sized + Send,
-    {
-        async move {
-            // Ensure that exiting causes the rest of the app to shut down
-            let _drop_guard = cancellation_token.clone().drop_guard();
-            self.run_without_cancellation().await
-        }
-    }
-
-    /// Meant to be called from `run` or if you really don't want automatic cancellation support
-    fn run_without_cancellation(self) -> impl Future<Output = anyhow::Result<()>> + Send;
-}
+pub use init::{server_init, ServerInit, ServerTask};
