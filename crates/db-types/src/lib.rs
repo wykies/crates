e373b0ce4b65e7d7a079d86848a92eedf5f74eaa
One <43485962+c-git@users.noreply.github.com>
2024-12-16 10:24:35 -0500

    feat: remove commented out code left in err

--- crates/db-types/src/lib.rs
+++ crates/db-types/src/lib.rs
@@ -1,6 +1,3 @@
-// #[cfg_attr(all(), path = "db_types_mysql.rs")]
-// pub mod db_types;
-
 #[cfg(not(feature = "mysql"))]
 compile_error!("No Database Type selected. Please use create features to select a DB to use");
 
