178e9b948609582f23aae55ad5e5d8fef72191a9
One <43485962+c-git@users.noreply.github.com>
2024-12-22 21:34:08 -0500

    chore: drop write time
    Lowered in case cancellation token is not signalled.
    Doesn't seem to have been when shutdown on shuttle.

--- crates/plugin-chat/src/consts.rs
+++ crates/plugin-chat/src/consts.rs
@@ -11,7 +11,7 @@ pub const CHAT_HISTORY_REQUEST_SIZE: u8 = 50;
 /// NOTE: Should be less than `CHAT_HISTORY_RECENT_CAPACITY`
 /// otherwise some history will not be accessible to recently joined users
 pub const CHAT_MAX_IMS_BEFORE_SAVE: u8 = 80;
-pub const CHAT_MAX_TIME_BEFORE_SAVE: Seconds = Seconds::new(30);
+pub const CHAT_MAX_TIME_BEFORE_SAVE: Seconds = Seconds::new(20);
 // TODO 6: Rate limit on server end
 pub const CHAT_MIN_TIME_BETWEEN_HISTORY_REQUESTS: Seconds = Seconds::new(5);
 pub const CHAT_SYSTEM_USERNAME: &str = "System";
