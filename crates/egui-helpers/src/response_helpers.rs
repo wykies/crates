80be6c581be224f831c8da59c1f3fc3ce2d96207
One <43485962+c-git@users.noreply.github.com>
2025-01-16 10:04:59 -0500

    chore: remove unnecessary mut

--- crates/egui-helpers/src/response_helpers.rs
+++ crates/egui-helpers/src/response_helpers.rs
@@ -3,11 +3,11 @@ use egui::Response;
 /// Adds convenance functions to [`egui::Response`]
 pub trait ResponseHelpers {
     /// Returns true if the Response lost focus and the enter key was pressed
-    fn enter_pressed(&mut self, ui: &egui::Ui) -> bool;
+    fn enter_pressed(&self, ui: &egui::Ui) -> bool;
 }
 
 impl ResponseHelpers for Response {
-    fn enter_pressed(&mut self, ui: &egui::Ui) -> bool {
+    fn enter_pressed(&self, ui: &egui::Ui) -> bool {
         self.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter))
     }
 }
