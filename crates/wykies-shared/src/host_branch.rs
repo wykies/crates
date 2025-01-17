1b0da2f83464fc9983c0593233e50d29ac7c82e7
One <43485962+c-git@users.noreply.github.com>
2025-01-17 12:26:10 -0500

    refactor: replace HostId with macro

--- crates/wykies-shared/src/host_branch.rs
+++ crates/wykies-shared/src/host_branch.rs
@@ -1,53 +1,11 @@
-use std::fmt::Display;
+use crate::{errors::ConversionError, id::DbId, string_wrapper};
 
-use crate::{errors::ConversionError, id::DbId};
+string_wrapper!(HostId, 50);
 
 #[derive(Debug, serde::Serialize, serde::Deserialize, Clone, PartialEq, Eq)]
 pub struct HostBranchPair {
     pub host_id: HostId,
     pub branch_id: DbId,
-}
-
-#[derive(Debug, serde::Serialize, serde::Deserialize, Clone, PartialEq, Eq)]
-pub struct HostId(String);
-
-impl HostId {
-    const MAX_LENGTH: usize = 50;
-}
-
-impl AsRef<str> for HostId {
-    fn as_ref(&self) -> &str {
-        &self.0
-    }
-}
-
-impl Display for HostId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-impl TryFrom<String> for HostId {
-    type Error = ConversionError;
-
-    fn try_from(value: String) -> Result<Self, Self::Error> {
-        if value.is_empty() {
-            return Err(ConversionError::Empty);
-        }
-        if value.len() > Self::MAX_LENGTH {
-            return Err(ConversionError::MaxExceeded {
-                max: Self::MAX_LENGTH,
-                actual: value.len(),
-            });
-        }
-        Ok(Self(value))
-    }
-}
-
-impl From<HostId> for String {
-    fn from(value: HostId) -> Self {
-        value.0
-    }
 }
 
 #[cfg(not(target_arch = "wasm32"))]
@@ -70,44 +28,5 @@ impl TryFrom<actix_web::dev::ConnectionInfo> for HostId {
             bail!("No 'peer_addr' found");
         };
         addr.to_string().try_into().context("invalid host_id")
-    }
-}
-
-#[cfg(feature = "server_only")]
-pub mod sql {
-    use super::*;
-    use crate::db_types::Db;
-
-    impl sqlx::Encode<'_, Db> for HostId {
-        fn encode_by_ref(
-            &self,
-            buf: &mut <Db as sqlx::Database>::ArgumentBuffer<'_>,
-        ) -> Result<sqlx::encode::IsNull, sqlx::error::BoxDynError> {
-            <String as sqlx::Encode<'_, Db>>::encode_by_ref(&self.0, buf)
-        }
-    }
-
-    impl sqlx::Type<Db> for HostId {
-        fn type_info() -> <Db as sqlx::Database>::TypeInfo {
-            <String as sqlx::Type<Db>>::type_info()
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use rstest::rstest;
-
-    use super::*;
-
-    #[rstest]
-    #[case::empty("", ConversionError::Empty)]
-    #[case::too_long("b".repeat(51), ConversionError::MaxExceeded{max:50, actual:51})]
-    fn illegal_role_names(#[case] name: String, #[case] expect: ConversionError) {
-        // Act
-        let actual: Result<HostId, ConversionError> = name.try_into();
-
-        // Assert
-        assert_eq!(actual.unwrap_err(), expect);
     }
 }
