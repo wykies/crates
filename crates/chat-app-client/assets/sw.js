fb3fa80ad791278c9a384c59fa7d13c0acfa0453
One <43485962+c-git@users.noreply.github.com>
2025-01-17 21:05:48 -0500

    style: apply dprint

--- crates/chat-app-client/assets/sw.js
+++ crates/chat-app-client/assets/sw.js
@@ -1,25 +1,25 @@
-var cacheName = 'chat-app-client-pwa';
+var cacheName = "chat-app-client-pwa";
 var filesToCache = [
-  './',
-  './index.html',
-  './chat-app-client.js',
-  './chat-app-client_bg.wasm',
+  "./",
+  "./index.html",
+  "./chat-app-client.js",
+  "./chat-app-client_bg.wasm",
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function (e) {
+self.addEventListener("install", function(e) {
   e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
+    caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
-    })
+    }),
   );
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function (e) {
+self.addEventListener("fetch", function(e) {
   e.respondWith(
-    caches.match(e.request).then(function (response) {
+    caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
-    })
+    }),
   );
 });
