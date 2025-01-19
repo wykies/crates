5a2849368df6606d820875fb7fc3571756e03edd
One <43485962+c-git@users.noreply.github.com>
2025-01-19 16:19:41 -0500

    chore: deprioritize test/feature not need for prod

--- crates/wykies-server/src/routes/admin/user.rs
+++ crates/wykies-server/src/routes/admin/user.rs
@@ -123,7 +123,7 @@ pub async fn user_update(
     pool: web::Data<DbPool>,
     wrapped: web::Json<RonWrapper>,
 ) -> actix_web::Result<actix_web::HttpResponse> {
-    // TODO 2: Ensure there is a test that assigns a role, changes a role and
+    // TODO 5: Ensure there is a test that assigns a role, changes a role and
     //          removes a role
     let pool: &DbPool = &pool;
     let diff: UserMetadataDiff = wrapped
