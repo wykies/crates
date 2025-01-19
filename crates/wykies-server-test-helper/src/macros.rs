7e05335f0431df3a58dd508a6787ca3e723bd0da
One <43485962+c-git@users.noreply.github.com>
2025-01-18 23:40:32 -0500

    test: change case of message for readability

--- crates/wykies-server-test-helper/src/macros.rs
+++ crates/wykies-server-test-helper/src/macros.rs
@@ -4,6 +4,6 @@ macro_rules! expect_ok {
     ($arg: expr) => {
         $arg.await
             .expect("failed to receive on rx")
-            .expect("result was not ok")
+            .expect("result was not Ok")
     };
 }
