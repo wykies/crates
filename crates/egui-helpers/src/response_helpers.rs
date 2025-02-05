52b09c421f75964c4271e4f779c71a3f7a8832b6
One <43485962+c-git@users.noreply.github.com>
2025-02-05 17:49:09 -0500

    feat: add convenience fn for cancel pressed

--- crates/egui-helpers/src/response_helpers.rs
+++ crates/egui-helpers/src/response_helpers.rs
@@ -4,10 +4,15 @@ use egui::Response;
 pub trait ResponseHelpers {
     /// Returns true if the Response lost focus and the enter key was pressed
     fn enter_pressed(&self, ui: &egui::Ui) -> bool;
+    /// Returns true if the Response lost focus and the escape key was pressed
+    fn escape_pressed(&self, ui: &egui::Ui) -> bool;
 }
 
 impl ResponseHelpers for Response {
     fn enter_pressed(&self, ui: &egui::Ui) -> bool {
         self.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter))
+    }
+    fn escape_pressed(&self, ui: &egui::Ui) -> bool {
+        self.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Escape))
     }
 }
