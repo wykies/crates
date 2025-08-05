bc4918cfc976daa821de2b62c34e4686031cb195
One <43485962+c-git@users.noreply.github.com>
2025-08-05 10:57:08 -0400

    chore:  update edition

--- scripts/switch_db.rs
+++ scripts/switch_db.rs
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S cargo +nightly -Zscript
 ---cargo
-package.edition = "2021"
+package.edition = "2024"
 [dependencies]
 switch-db ={version = "*", path = "../crates/switch-db"}
 anyhow = "1.0.94"
