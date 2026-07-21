7997ff2a78e7164441649a23f525f1de07e6a6b2
One <43485962+c-git@users.noreply.github.com>
2026-07-20 21:33:50 -0400

    feat: add const function to convert letters to col number

--- crates/umya-helper/src/lib.rs
+++ crates/umya-helper/src/lib.rs
@@ -261,3 +261,35 @@ pub fn set_frozen_pane(
     pane.set_state(umya_spreadsheet::PaneStateValues::Frozen);
     Ok(())
 }
+
+/// If using from a const context see `const_only_alpha_to_index`
+pub const fn alpha_to_index(col: &str) -> Option<u32> {
+    let bytes = col.as_bytes();
+    let mut num = 0;
+    let mut i = 0;
+    if bytes.is_empty() {
+        return None;
+    }
+    while i < bytes.len() {
+        let byte = bytes[i];
+        let val = if byte >= b'A' && byte <= b'Z' {
+            (byte - b'A' + 1) as u32
+        } else if byte >= b'a' && byte <= b'z' {
+            (byte - b'a' + 1) as u32
+        } else {
+            return None; // Invalid character
+        };
+        num = num * 26 + val;
+        i += 1;
+    }
+    Some(num)
+}
+
+/// Const version that panics on invalid input so only intended to be used in
+/// const context, use `alpha_to_index` if not being used in a const context
+pub const fn const_only_alpha_to_index(col: &str) -> u32 {
+    match alpha_to_index(col) {
+        Some(x) => x,
+        None => panic!("Invalid Column Letter"),
+    }
+}
